b8b38df2eed43a02ed69c022d1534b4ff47976fe
unaisshemim <unaisshemim@gmail.com>
2023-07-11 10:21:02 +0530

    changed the work name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             Hi ,I'am <span className="text-[#8e65e7] ">UNAIZ</span>
           </h1>
           <p className={`${styles.heroSubText} mt-5  text-white-500`}>
-            I develop 3d visual,user <br className="sm:block hidden" />
+            I develop user <br className="sm:block hidden" />
             Interface and web application{" "}
           </p>
         </div>
